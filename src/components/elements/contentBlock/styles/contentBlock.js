4d223735b512d830061d2bc8f29057c9a89c1d51
leonardo <leon.ak@hotmail.com>
2019-12-19 16:03:54 -0300

    implementado content block

---
+++ src/components/elements/contentBlock/styles/contentBlock.js
@@ -0,0 +1,14 @@
+import {
+  StyleSheet,
+} from 'react-native';
+
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+export default StyleSheet.create({
+  contentBlock: {
+    margin: 16,
+    color: Colors.dark,
+  },
+});
