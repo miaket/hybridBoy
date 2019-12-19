b1d73a84c31f5ad04ad22c1385277c545be67a87
leonardo <leon.ak@hotmail.com>
2019-12-19 16:02:53 -0300

    implementado input text

---
+++ src/components/elements/inputText/InputText.native.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Input, Item, Label } from 'native-base';
+
+const mainTitle = ({
+  inputLabel,
+  inputType
+}) => {
+  const secureTextEntry = inputType === 'password' ? true : false
+  ;
+  return (
+    <>
+      <Item stackedLabel>
+        <Label>
+          { inputLabel }
+        </Label>
+        <Input secureTextEntry={secureTextEntry} />
+      </Item>
+    </>
+  )
+};
+
+mainTitle.propTypes = {
+};
+
+mainTitle.defaultProps = {
+};
+
+export default mainTitle;
