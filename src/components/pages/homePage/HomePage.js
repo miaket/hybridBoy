ce9ecb174d46cc3a6aec46d15cee6073cdf8696b
leonardo <leon.ak@hotmail.com>
2019-12-19 21:56:11 -0300

    remocao de import

--- src/components/pages/homePage/HomePage.js
+++ src/components/pages/homePage/HomePage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import HomePageContent from './content/HomePageContent';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
